982558c86f1c0e9f8850f2b09e57f110a16bad1a
Paulo Peixoto <pauloeduardo_peixoto@hotmail.com>
2021-08-05 19:18:15 -0300

    feat(model): Add Category
    feat(routes): Add Categories.

--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -1,18 +1,24 @@
 import { Router } from 'express';
+import { Category } from '../model/Category';
 
 const categoriesRoutes = Router();
 
-const categories = [];
+const categories: Category[]= [];
 
-categoriesRoutes.post('/categories', (request, response) => {
+categoriesRoutes.post('/', (request, response) => {
     const { name, description } = request.body;
 
-    categories.push({
+    const category = new Category(); 
+    
+    Object.assign(category, {
         name,
-        description
+        description,
+        created_at: new Date()
     });
+        
+    categories.push(category);
 
-    return response.status(201).send();
-})
+    return response.status(201).json({category});
+});
 
 export { categoriesRoutes };
