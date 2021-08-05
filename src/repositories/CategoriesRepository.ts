45ee077163f96fc9b2141dec160a632868f07d96
Paulo Peixoto <pauloeduardo_peixoto@hotmail.com>
2021-08-05 20:07:43 -0300

    feat(Repositories): init - video 04:04

---
+++ src/repositories/CategoriesRepository.ts
@@ -0,0 +1,9 @@
+import { Category } from '../model/Category'
+
+class CategoriesRepository {
+
+    private categories: Category[]= [];
+
+};
+
+export { CategoriesRepository };
--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -3,7 +3,7 @@ import { Category } from '../model/Category';
 
 const categoriesRoutes = Router();
 
-const categories: Category[]= [];
+
 
 categoriesRoutes.post('/', (request, response) => {
     const { name, description } = request.body;
