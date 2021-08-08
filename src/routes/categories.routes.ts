f8e3c2eb585bbe3a48a0a5f3a957371d7ea4bf36
Paulo Peixoto <pauloeduardo_peixoto@hotmail.com>
2021-08-08 18:29:24 -0300

    feat(Controllers): add to Project.

--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -1,18 +1,13 @@
 import { Router } from 'express';
 import { CategoriesRepository } from '../modules/cars/repositories/CategoriesRepository';
-import { CreateCategoryService } from '../modules/cars/services/CreateCategoryService';
+import { CreateCategoryUseCase } from '../modules/cars/useCases/createCategory/CreateCategoryUseCase';
 
 const categoriesRoutes = Router();
 const categoriesRepository = new CategoriesRepository();
 
 
 categoriesRoutes.post('/', (request, response) => {
-    const { name, description } = request.body;
 
-    const createCategoryService = new CreateCategoryService(categoriesRepository);
-    createCategoryService.execute({ name, description });
-
-    return response.status(201).send();
 });
 
 categoriesRoutes.get('/', (request, response) => {
