d0fe128c7697949f871d566ed45a393c6016e17a
Paulo Peixoto <pauloeduardo_peixoto@hotmail.com>
2021-08-08 18:40:08 -0300

    refactor(Category Route Post): uso da Controller na rota.

--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -1,13 +1,12 @@
 import { Router } from 'express';
 import { CategoriesRepository } from '../modules/cars/repositories/CategoriesRepository';
-import { CreateCategoryUseCase } from '../modules/cars/useCases/createCategory/CreateCategoryUseCase';
+import { createCategoryController } from '../modules/cars/useCases/createCategory';
 
 const categoriesRoutes = Router();
 const categoriesRepository = new CategoriesRepository();
 
-
 categoriesRoutes.post('/', (request, response) => {
-
+    return createCategoryController.handle(request, response);
 });
 
 categoriesRoutes.get('/', (request, response) => {
