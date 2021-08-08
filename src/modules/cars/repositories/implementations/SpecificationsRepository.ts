a499e3119cd2dc15d6d06c40b37ea3e1c9320da3
Paulo Peixoto <pauloeduardo_peixoto@hotmail.com>
2021-08-08 20:08:23 -0300

    refactor(dir implementations): add to Project.

--- src/modules/cars/repositories/implementations/SpecificationsRepository.ts
+++ src/modules/cars/repositories/implementations/SpecificationsRepository.ts
@@ -1,5 +1,5 @@
-import { ICreateSpecificationDTO, ISpecificationsRepository } from "./ISpecificatonsRepository";
-import { Specification } from '../model/Specification'
+import { ICreateSpecificationDTO, ISpecificationsRepository } from "../ISpecificatonsRepository";
+import { Specification } from '../../model/Specification'
 
 
 class SpecificationsRepository implements ISpecificationsRepository {
