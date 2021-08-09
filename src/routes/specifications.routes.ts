63e5d4c5732ee97ac865adb590cc0b3115614487
Paulo Peixoto <pauloeduardo_peixoto@hotmail.com>
2021-08-09 10:28:08 -0300

    refactor(Specification): Add Controller to Project.
    refactor(Routes Specification): with the implementation of the Controller.

--- src/routes/specifications.routes.ts
+++ src/routes/specifications.routes.ts
@@ -1,18 +1,10 @@
 import { Router } from 'express';
-import { CreateSpecificationService } from '../modules/cars/services/CreateSpecificationService';
-import { SpecificationsRepository } from '../modules/cars/repositories/implementations/SpecificationsRepository';
+import { createSpecificationController } from '../modules/cars/useCases/createSpecification';
 
 const specificationsRoutes = Router();
 
-const specificationsRepository = new SpecificationsRepository();
-
 specificationsRoutes.post('/', (request, response) => {
-    const { name, description } = request.body;
-
-    const createSpecificationService = new CreateSpecificationService(specificationsRepository);
-    createSpecificationService.execute({ name, description });
-
-    return response.status(201).send();
+    return createSpecificationController.handle(request, response);
 });
 
 export { specificationsRoutes };
