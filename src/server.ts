dd4a93c51d49d8add6c2f84b3f0383261f6951a4
Paulo Peixoto <pauloeduardo_peixoto@hotmail.com>
2021-08-09 16:56:05 -0300

    feat(Swagger): add to Project. Swagger implementation for API documentation elaboration.

--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,12 @@
 import express from 'express';
+import swagger from 'swagger-ui-express';
 import { router } from './routes';
+import swaggerConfig from './swagger.json';
 
 const app = express();
 app.use(express.json());
+
+app.use('/api-docs', swagger.serve, swagger.setup(swaggerConfig));
 
 app.use(router);
 
