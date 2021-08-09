3353fd57dcf6e130e45b4786ecf0db2ed9c200cb
Paulo Peixoto <pauloeduardo_peixoto@hotmail.com>
2021-08-09 14:41:02 -0300

    refactor(ImportCategoryUseCase.ts): improves the code so that the reading of the file is performed.

--- src/modules/cars/useCases/importCategory/ImportCategoryController.ts
+++ src/modules/cars/useCases/importCategory/ImportCategoryController.ts
@@ -1,8 +1,6 @@
 import { Request, Response } from 'express';
 import { ImportCategoryUseCase } from './ImportCategoryUseCase';
 
-
-
 class ImportCategoryController {
     constructor(private importCategoryUseCase: ImportCategoryUseCase) {
 
