e6480692f942d1ad7f1548eb453999acb01ccbb6
Paulo Eduardo Peixoto <pauloeduardo_peixoto@hotmail.com>
2021-09-15 19:29:28 -0300

    feat(AppError): change sample Error for AppError.
    FILE: ensureAuthenticate.ts.

--- src/middlewares/ensureAuthenticated.ts
+++ src/middlewares/ensureAuthenticated.ts
@@ -1,5 +1,6 @@
 import { Request, Response, NextFunction } from "express";
 import { verify } from 'jsonwebtoken';
+import { AppError } from "../errors/AppError";
 import { UsersRepository } from "../modules/accounts/repositories/implementations/UsersRepository";
 
 interface IPayload {
@@ -11,7 +12,7 @@ export async function ensureAuthenticated(request: Request, response: Response, 
     const authHeader = request.headers.authorization;
 
     if(!authHeader){
-        throw new Error("Token Missing!")
+        throw new AppError("Token Missing!", 401)
     };
 
     // O Bearer será separado do corpo do Token
@@ -25,12 +26,12 @@ export async function ensureAuthenticated(request: Request, response: Response, 
 
         const user = usersRepository.findById(user_id);
         if (!user) {
-            throw new Error("User does not exists!")
+            throw new AppError("User does not exists!", 401)
         }
 
         next();
     } catch {
-        throw new Error("Invalid Token!")
+        throw new AppError("Invalid Token!", 401)
     } 
 
 }
