880c1b17c63272b6efad6d4e55a834ff4a5d80e0
Paulo Eduardo Peixoto <pauloeduardo_peixoto@hotmail.com>
2021-09-18 15:26:05 -0300

    feat(deleteFile): function added to the Project.

--- src/modules/accounts/useCases/updateUserAvatar/UpdateUserAvatarUseCase.ts
+++ src/modules/accounts/useCases/updateUserAvatar/UpdateUserAvatarUseCase.ts
@@ -1,4 +1,5 @@
 import { inject, injectable } from "tsyringe";
+import { deleteFile } from "../../../../utils/file";
 import { IUsersRepository } from "../../repositories/IUsersRepository";
 
 interface IRequest {
@@ -17,6 +18,14 @@ class UpdateUserAvatarUseCase {
     async execute({ user_id, avatar_file }: IRequest): Promise<void> {
         const user = await this.usersRepository.findById(user_id);
 
+        // Verifica se já existe um Avatar inserido para o usuário
+        if(user.avatar) {
+            // Realiza a Busca do Avatar no Banco de Dados
+            // Há a necessidade de se indicar o diretório que o arquivo deve ser buscado para que ele seja apagado
+            await deleteFile(`./tmp/avatar/${user.avatar}`);
+        };
+
+        // Substituição do Avatar (caso já exista), ou inserção no caso de Novo
         user.avatar = avatar_file;
 
         await this.usersRepository.create(user);
