a341b6fc5d5b3d771e4b92100b6896e8eb6e0e58
Paulo Eduardo Peixoto <pauloeduardo_peixoto@hotmail.com>
2021-09-20 19:29:15 -0300

    feat(Repositories/In-Memory): create UsersRepository.
    For Tests with Jest.

---
+++ src/modules/accounts/repositories/in-memory/UsersRepositoryInMemory.ts
@@ -0,0 +1,33 @@
+import { ICreateUserDTO } from "../../dtos/ICreateUserDTO";
+import { User } from "../../entities/User";
+import { IUsersRepository } from "../IUsersRepository";
+
+
+
+class UsersRepositoryInMemory implements IUsersRepository {
+
+    users: User[] = [];
+
+    async create({name, email, password, driver_license}: ICreateUserDTO): Promise<void> {
+        
+        const newUser = new User();
+        
+        Object.assign(newUser, {
+            name,
+            email,
+            password,
+            driver_license,
+        });
+
+        this.users.push(newUser);
+    };
+
+    async findByEmail(email: string): Promise<User> {
+        return this.users.find((findUser) => findUser.email === email);
+    };
+
+    async findById(id: string): Promise<User> {
+        return this.users.find((findUser) => findUser.id === id);
+    }
+
+}
