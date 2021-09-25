f53835b245d778fad3886559254a0537812eda5c
Paulo Eduardo Peixoto <pauloeduardo_peixoto@hotmail.com>
2021-09-25 18:59:16 -0300

    feat(Container): Added CarsRepository.
    NOTE: Tsyringe.

--- src/shared/container/index.ts
+++ src/shared/container/index.ts
@@ -5,6 +5,8 @@ import { ISpecificationsRepository } from '@modules/cars/repositories/ISpecifica
 import { SpecificationsRepository } from '@modules/cars/infra/typeorm/repositories/SpecificationsRepository';
 import { IUsersRepository } from '@modules/accounts/repositories/IUsersRepository';
 import { UsersRepository } from '@modules/accounts/infra/typeorm/repositories/UsersRepository';
+import { ICarsRepository } from '@modules/cars/repositories/ICarsRepository';
+import { CarsRepository } from '@modules/cars/infra/typeorm/repositories/CarsRepository';
 
 // ICategoriesRepository
 container.registerSingleton<ICategoriesRepository>(
@@ -22,4 +24,10 @@ container.registerSingleton<ISpecificationsRepository>(
 container.registerSingleton<IUsersRepository>(
     "UsersRepository",
     UsersRepository
+);
+
+// ICarRepository
+container.registerSingleton<ICarsRepository>(
+    "CarsRepository",
+    CarsRepository
 );
