423e6b7bfa292d09f3262a5cf976586eb20eddea
Paulo Eduardo Peixoto <pauloeduardo_peixoto@hotmail.com>
2021-09-25 19:13:08 -0300

    feat(Cars): Create Routes.

--- src/shared/infra/http/routes/index.ts
+++ src/shared/infra/http/routes/index.ts
@@ -3,12 +3,14 @@ import { authRoutes } from './auth.routes';
 import { categoriesRoutes } from './categories.routes'
 import { specificationsRoutes } from './specifications.routes';
 import { usersRoutes } from './user.routes';
+import { carsRoutes } from './cars.routes';
 
 const router = Router();
 
 router.use('/categories', categoriesRoutes);
 router.use('/specifications', specificationsRoutes);
 router.use('/users', usersRoutes);
+router.use('/cars', carsRoutes);
 router.use(authRoutes);
 
 export { router };
