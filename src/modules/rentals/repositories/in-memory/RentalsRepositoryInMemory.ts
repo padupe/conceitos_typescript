c63dad14e6a401f65046f845c4ee112588ec0c8e
Paulo Eduardo Peixoto <pauloeduardo_peixoto@hotmail.com>
2021-10-09 16:44:10 -0300

    fix(Rentals/Repositories): fix methods findRentalOpenByCAr and findrentalOpenByUser. Verifiy !rental.end_date.

--- src/modules/rentals/repositories/in-memory/RentalsRepositoryInMemory.ts
+++ src/modules/rentals/repositories/in-memory/RentalsRepositoryInMemory.ts
@@ -8,11 +8,11 @@ class RentalsRepositoryInMemory implements IRentalsRepository {
     rentals: Rental[] = [];
 
     async findRentalOpenByCar(car_id: string): Promise<Rental> {
-        return this.rentals.find(rental => rental.car_id === car_id && rental.end_date === null)
+        return this.rentals.find(rental => rental.car_id === car_id && !rental.end_date)
     };
 
     async findRentalOpenByUser(user_id: string): Promise<Rental> {
-        return this.rentals.find(rental => rental.user_id === user_id && rental.end_date === null)
+        return this.rentals.find(rental => rental.user_id === user_id && !rental.end_date)
     };
 
     async create({car_id, user_id, expected_return_date}: ICreateRentalDTO): Promise<Rental> {
