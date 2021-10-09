c0d789fab92240a55fea4f5fef2f11eebe4cabc7
Paulo Eduardo Peixoto <pauloeduardo_peixoto@hotmail.com>
2021-10-09 16:21:47 -0300

    test(Rentals): Create a Rental.

--- src/modules/rentals/useCases/createRental/CreateRentalUseCase.spec.ts
+++ src/modules/rentals/useCases/createRental/CreateRentalUseCase.spec.ts
@@ -1,16 +1,23 @@
+import { RentalsRepositoryInMemory } from "@modules/rentals/repositories/in-memory/RentalsRepositoryInMemory";
 import { CreateRentalUseCase } from "./CreateRentalUseCase"
 
 
 let createRentalUseCase: CreateRentalUseCase;
+let rentalsRepositoryInMemory: RentalsRepositoryInMemory;
 
 describe('Create Rental', () => {
 
     beforeEach(() => {
-        createRentalUseCase = new CreateRentalUseCase();
+        rentalsRepositoryInMemory = new RentalsRepositoryInMemory();
+        createRentalUseCase = new CreateRentalUseCase(rentalsRepositoryInMemory);
     });
 
     it('Should be able to create a new rental', async () => {
 
-        await createRentalUseCase.execute();
+        await createRentalUseCase.execute({
+            user_id: 'User123456',
+            car_id: 'Car123456',
+            expected_return_date: new Date(),
+        });
     });
 })
