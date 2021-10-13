eb367d173bc8d4565560830ebd4f11a038d6bdc4
Paulo Eduardo Peixoto <pauloeduardo_peixoto@hotmail.com>
2021-10-13 18:49:31 -0300

    refactor(Rentals/useCases): update CreateRentalUseCase.ts. Inject and Injectable added to file.

--- src/modules/rentals/useCases/createRental/CreateRentalUseCase.ts
+++ src/modules/rentals/useCases/createRental/CreateRentalUseCase.ts
@@ -2,8 +2,7 @@ import { Rental } from "@modules/rentals/infra/typeorm/entities/Rental";
 import { IRentalsRepository } from "@modules/rentals/repositories/IRentalsRepository";
 import { IDateProvider } from "@shared/container/providers/DateProvider/IDateProvider";
 import { AppError } from "@shared/errors/AppError";
-
-
+import { inject, injectable } from "tsyringe";
 
 interface IRequest {
     user_id: string;
@@ -11,10 +10,13 @@ interface IRequest {
     expected_return_date: Date;
 };
 
+@injectable()
 class CreateRentalUseCase {
 
     constructor(
+        @inject('RentalsRepository')
         private rentalsRepository: IRentalsRepository,
+        @inject('DayJSDateProvider')
         private dateProvider: IDateProvider,
     ) {}
 
