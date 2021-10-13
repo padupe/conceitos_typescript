dd2e84f51fb44e19e8e8e67f2b14f2d9f052f07f
Paulo Eduardo Peixoto <pauloeduardo_peixoto@hotmail.com>
2021-10-13 19:44:52 -0300

    refactor(Shared/Infra/Middlewares): Import ensureAuthenticated.ts in routes.

--- src/shared/infra/http/routes/rentals.routes.ts
+++ src/shared/infra/http/routes/rentals.routes.ts
@@ -1,6 +1,6 @@
 import { CreateRentalController } from '@modules/rentals/useCases/createRental/CreateRentalController';
 import { Router } from 'express';
-import { ensureAuthenticated } from 'middlewares/ensureAuthenticated';
+import { ensureAuthenticated } from '../middlewares/ensureAuthenticated';
 
 const rentalsRoutes = Router();
 
