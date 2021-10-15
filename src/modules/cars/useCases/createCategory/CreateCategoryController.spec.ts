2121a3e0087c8a17a5f56918546bbe1a481f0c32
Paulo Eduardo Peixoto <pauloeduardo_peixoto@hotmail.com>
2021-10-15 18:30:30 -0300

    test(Integration): CreateCategoryController added to the Project.

--- src/modules/cars/useCases/createCategory/CreateCategoryController.spec.ts
+++ src/modules/cars/useCases/createCategory/CreateCategoryController.spec.ts
@@ -39,13 +39,37 @@ describe('Create Category Controller', () => {
             password: 'admin'
         });
 
+        const { token } = responseToken.body;
+
+        const response = await request(app).post('/categories').send({
+            name: "Category Integration Test",
+            description: "Description Integration Test"
+        }).set({
+            Authorization: `Bearer ${token}`,
+        });
+
+        expect(response.status).toBe(201);
+    });
+
+    it('Should not be able to create a new category with name exists', async () => {
+
+        const responseToken = await request(app).post('/sessions').send({
+            email: 'admin@rentx.com.br',
+            password: 'admin'
+        });
+
+        const { token } = responseToken.body;
+
         console.log(responseToken.body)
 
         const response = await request(app).post('/categories').send({
             name: "Category Integration Test",
             description: "Description Integration Test"
+        }).set({
+            Authorization: `Bearer ${token}`,
         });
 
-        expect(response.status).toBe(201);
-    })
+        expect(response.status).toBe(400);
+    });
+
 })
