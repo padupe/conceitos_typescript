0ed89862adb5ce25c33c1bee9807caa16c631714
Paulo Eduardo Peixoto <pauloeduardo_peixoto@hotmail.com>
2021-10-18 20:55:45 -0300

    fix(ICarsRepository): fix call updateAvailable function.

--- src/modules/cars/repositories/ICarsRepository.ts
+++ src/modules/cars/repositories/ICarsRepository.ts
@@ -7,7 +7,7 @@ interface ICarsRepository {
     findByLicencePlate(license_plate: string): Promise<Car>;
     listAvailable(category_id?: string, brand?: string, name?: string): Promise<Car[]>;
     findById(id: string): Promise<Car>;
-    updateAvailabel(id: string, available: boolean): Promise<void>;
+    updateAvailable(id: string, available: boolean): Promise<void>;
 };
 
 export { ICarsRepository };
--- src/modules/rentals/useCases/devolutionRental/DevolutionRentalUseCase.ts
+++ src/modules/rentals/useCases/devolutionRental/DevolutionRentalUseCase.ts
@@ -1,10 +1,54 @@
+import { ICarsRepository } from "@modules/cars/repositories/ICarsRepository";
+import { IRentalsRepository } from "@modules/rentals/repositories/IRentalsRepository";
+import { IDateProvider } from "@shared/container/providers/DateProvider/IDateProvider";
+import { AppError } from "@shared/errors/AppError";
+import { inject } from "tsyringe";
 
-
+interface IRequest {
+    id: string;
+    user_id: string;
+};
 
 class DevolutionRentalUseCase {
 
-    async handle() {
+    constructor(
+        @inject('RentalsRepository')
+        private rentalsRepository: IRentalsRepository,
+        @inject('CarsRepository')
+        private carsRepository: ICarsRepository,
+        @inject('DayJSDateProvider')
+        private dateProvider: IDateProvider,
+    ){};
 
+    async handle({ id, user_id }: IRequest) {
+
+        const minDaily = 1;
+
+        // Localizar o id do Aluguel
+        const findRental = await this.rentalsRepository.findById(id);
+
+        if(!findRental) {
+            throw new AppError('Rental does not exists!')
+        };
+
+        // Verificar o Tempo do Aluguel
+        const dateNow = this.dateProvider.dateNow();
+
+        let daily = this.dateProvider.compareInDays(
+            findRental.start_date,
+            dateNow,
+        );
+
+        if(daily <= 0) {
+            daily = minDaily;
+        };
+
+        const delay = this.dateProvider.compareInDays(
+            dateNow,
+            findRental.expected_return_date
+        );
+
+        if(diffInHours < )
     };
 
 };
