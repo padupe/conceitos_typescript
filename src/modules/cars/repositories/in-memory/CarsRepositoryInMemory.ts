83c90ac3b870cf0842e5cb7204e897d49f3c1170
Paulo Eduardo Peixoto <pauloeduardo_peixoto@hotmail.com>
2021-10-18 20:56:56 -0300

    fix(CarsRepositoryInMemory): fix call updateAvailable function.

--- src/modules/cars/repositories/in-memory/CarsRepositoryInMemory.ts
+++ src/modules/cars/repositories/in-memory/CarsRepositoryInMemory.ts
@@ -48,7 +48,7 @@ class CarsRepositoryInMemory implements ICarsRepository {
         return this.cars.find((findCarId) => findCarId.id === id);
     };
 
-    async updateAvailabel(id: string, available: boolean): Promise<void> {
+    async updateAvailable(id: string, available: boolean): Promise<void> {
         const findIndex = this.cars.findIndex((car) => car.id === id);
         this.cars[findIndex].available = available;
     };
