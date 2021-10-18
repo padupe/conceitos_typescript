82b48580fb0b63434b73caa1d5b40bc55aad291f
Paulo Eduardo Peixoto <pauloeduardo_peixoto@hotmail.com>
2021-10-18 19:40:32 -0300

    feat(ICarsRepository): updateAvailable added to the interface.

--- src/modules/cars/repositories/ICarsRepository.ts
+++ src/modules/cars/repositories/ICarsRepository.ts
@@ -7,6 +7,7 @@ interface ICarsRepository {
     findByLicencePlate(license_plate: string): Promise<Car>;
     listAvailable(category_id?: string, brand?: string, name?: string): Promise<Car[]>;
     findById(id: string): Promise<Car>;
+    updateAvailabel(id: string): Promise<Car>;
 };
 
 export { ICarsRepository };
