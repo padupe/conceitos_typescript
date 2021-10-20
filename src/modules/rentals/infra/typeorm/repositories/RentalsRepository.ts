88e5f7658b81c01be6dc73e888184b7d7dceb7a4
Paulo Eduardo Peixoto <pauloeduardo_peixoto@hotmail.com>
2021-10-19 21:52:34 -0300

    refactor(RentalsRepository): findRentalOpenByCar and findRentalOperByUser functions.

--- src/modules/rentals/infra/typeorm/repositories/RentalsRepository.ts
+++ src/modules/rentals/infra/typeorm/repositories/RentalsRepository.ts
@@ -14,12 +14,16 @@ class RentalsRepository implements IRentalsRepository{
 ;
 
     async findRentalOpenByCar(car_id: string): Promise<Rental> {
-        const openByCar = await this.repository.findOne({car_id});
+        const openByCar = await this.repository.findOne({
+            where: { car_id, end_date: null },
+        });
         return openByCar;
     };
 
     async findRentalOpenByUser(user_id: string): Promise<Rental> {
-        const openByUser = await this.repository.findOne({user_id});
+        const openByUser = await this.repository.findOne({
+            where: { user_id, end_date: null },
+        });
         return openByUser;
     };
 
