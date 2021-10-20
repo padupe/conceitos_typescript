576fbb17fa1575056b361b8fbfa38a4c2086cb12
Paulo Eduardo Peixoto <pauloeduardo_peixoto@hotmail.com>
2021-10-19 21:26:21 -0300

    feat(DevolutionRentalUseCase): insert @injectable() in code.

--- src/modules/rentals/useCases/devolutionRental/DevolutionRentalUseCase.ts
+++ src/modules/rentals/useCases/devolutionRental/DevolutionRentalUseCase.ts
@@ -3,13 +3,14 @@ import { Rental } from "@modules/rentals/infra/typeorm/entities/Rental";
 import { IRentalsRepository } from "@modules/rentals/repositories/IRentalsRepository";
 import { IDateProvider } from "@shared/container/providers/DateProvider/IDateProvider";
 import { AppError } from "@shared/errors/AppError";
-import { inject } from "tsyringe";
+import { inject, injectable } from "tsyringe";
 
 interface IRequest {
     id: string;
     user_id: string;
 };
 
+@injectable()
 class DevolutionRentalUseCase {
 
     constructor(
