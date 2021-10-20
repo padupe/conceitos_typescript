aa055f50502792e98eb6e30e0230f9523feac6cc
Paulo Eduardo Peixoto <pauloeduardo_peixoto@hotmail.com>
2021-10-19 21:43:15 -0300

    refacotr9RentalsRepository): for correctly use.

--- src/modules/rentals/infra/typeorm/repositories/RentalsRepository.ts
+++ src/modules/rentals/infra/typeorm/repositories/RentalsRepository.ts
@@ -23,13 +23,14 @@ class RentalsRepository implements IRentalsRepository{
         return openByUser;
     };
 
-    async create({ car_id, user_id, expected_return_date, id, end_date }: ICreateRentalDTO): Promise<Rental> {
+    async create({ car_id, user_id, expected_return_date, id, end_date, total }: ICreateRentalDTO): Promise<Rental> {
         const newRental = this.repository.create({
             user_id,
             car_id,
             expected_return_date,
             id,
             end_date,
+            total,
         });
 
         this.repository.save(newRental);
