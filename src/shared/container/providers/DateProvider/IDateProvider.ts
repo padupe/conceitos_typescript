dc86cd0a7eedaf1cc26419542f38e48aa9d9da9a
Paulo Eduardo Peixoto <pauloeduardo_peixoto@hotmail.com>
2021-10-30 15:13:29 -0300

    feat(IDateProvider): Added addDays function.

--- src/shared/container/providers/DateProvider/IDateProvider.ts
+++ src/shared/container/providers/DateProvider/IDateProvider.ts
@@ -1,5 +1,5 @@
 interface IDateProvider {
-    addDays(days: number);
+    addDays(days: number): Date;
     compareInHours(start_date: Date, end_date: Date): number;
     compareInDays(start_date: Date, end_date: Date): number;
     convertToUTC(date: Date): string;
