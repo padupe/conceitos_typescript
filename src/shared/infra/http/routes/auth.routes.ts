be2386d887bbe1d02ffda22f3bf703c3e696043a
Paulo Eduardo Peixoto <pauloeduardo_peixoto@hotmail.com>
2021-10-30 17:21:25 -0300

    feat(Routes/Auth): Added pah refresh-token.

--- src/shared/infra/http/routes/auth.routes.ts
+++ src/shared/infra/http/routes/auth.routes.ts
@@ -1,10 +1,13 @@
 import { Router } from 'express';
 import { AuthUserController } from '@modules/accounts/useCases/auth/AuthUserController';
+import { RefreshTokenController } from '@modules/accounts/useCases/refreshToken/RefreshTokenController';
 
 const authRoutes = Router();
 const authUserController = new AuthUserController();
+const refreshTokenController = new RefreshTokenController();
 
-authRoutes.post("/sessions", authUserController.handle)
+authRoutes.post("/sessions", authUserController.handle);
+authRoutes.post("/refresh-token", refreshTokenController.handle);
 
 
 export { authRoutes };
