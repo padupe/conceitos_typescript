33cee88a376fde76b887fe885b3a2d3bbd3080e5
Paulo Eduardo Peixoto <pauloeduardo_peixoto@hotmail.com>
2021-10-30 15:14:12 -0300

    feat(DayJSDateProvider): Added addDays function.

--- src/shared/container/providers/DateProvider/implementations/DayJSDateProvider.ts
+++ src/shared/container/providers/DateProvider/implementations/DayJSDateProvider.ts
@@ -7,6 +7,10 @@ dayjs.extend(utc);
 
 class DayJSDateProvider implements IDateProvider {
     
+    addDays(days: number): Date {
+        return dayjs().add(days, 'days').toDate();
+    }
+    
     compareInHours(start_date: Date, end_date: Date): number {
         const start_date_utc = this.convertToUTC(start_date);
         const end_date_utc = this.convertToUTC(end_date);
