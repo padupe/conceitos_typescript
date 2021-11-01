85141ae7f802b6a8cb01f358e60b4832df2a3514
Paulo Eduardo Peixoto <pauloeduardo_peixoto@hotmail.com>
2021-11-01 18:42:22 -0300

    feat(SendForgotPasswordMailUseCase): Initial Implementation. @inject(EtherealMailProvider).

--- src/modules/accounts/useCases/SendForgotPasswordMail/SendForgotPasswordMailUseCase.ts
+++ src/modules/accounts/useCases/SendForgotPasswordMail/SendForgotPasswordMailUseCase.ts
@@ -1,6 +1,7 @@
 import { IUsersRepository } from "@modules/accounts/repositories/IUsersRepository";
 import { IUsersTokensRepository } from "@modules/accounts/repositories/IUsersTokensRepository";
 import { IDateProvider } from "@shared/container/providers/DateProvider/IDateProvider";
+import { IMailProvider } from "@shared/container/providers/MailProvider/IMailProvider";
 import { AppError } from "@shared/errors/AppError";
 import { inject, injectable } from "tsyringe";
 import { v4 as uuidv4 } from 'uuid';
@@ -16,7 +17,10 @@ class SendForgotPasswordMailUseCase {
         private usersTokensRepository: IUsersTokensRepository,
 
         @inject("DayJSDateProvider")
-        private dayJSDateProvider: IDateProvider
+        private dayJSDateProvider: IDateProvider,
+
+        @inject( "EtherealMailProvider")
+        private mailProvider: IMailProvider
     ){}
 
     async execute(email: string) {
