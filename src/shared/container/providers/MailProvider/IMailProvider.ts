e35f25047fd9a2299bc825dcf50824d2e228b88f
Paulo Eduardo Peixoto <pauloeduardo_peixoto@hotmail.com>
2021-11-03 20:57:17 -0300

    feat(IMailProvider): New structure.

--- src/shared/container/providers/MailProvider/IMailProvider.ts
+++ src/shared/container/providers/MailProvider/IMailProvider.ts
@@ -1,7 +1,7 @@
 
 
 interface IMailProvider {
-    sendMail(to: string, subject: string, body: string): Promise<void>;
+    sendMail(to: string, subject: string, variables: any, path: string): Promise<void>;
 };
 
 export { IMailProvider };
