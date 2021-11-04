afd51acefe7bdd97ee5bf31ae7e79c1d26ed6a90
Paulo Eduardo Peixoto <pauloeduardo_peixoto@hotmail.com>
2021-11-03 21:40:12 -0300

    feat(IDateProvider): Added compareIfBefore method.

--- src/shared/container/providers/DateProvider/IDateProvider.ts
+++ src/shared/container/providers/DateProvider/IDateProvider.ts
@@ -1,6 +1,7 @@
 interface IDateProvider {
     addDays(days: number): Date;
     addHours(hours: number): Date;
+    compareIfBefore(stard_date: Date, end_date: Date): boolean;
     compareInHours(start_date: Date, end_date: Date): number;
     compareInDays(start_date: Date, end_date: Date): number;
     convertToUTC(date: Date): string;
