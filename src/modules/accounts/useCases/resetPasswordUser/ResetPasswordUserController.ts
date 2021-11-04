344a126fd49682e38aa57346587a8e8b0fd08174
Paulo Eduardo Peixoto <pauloeduardo_peixoto@hotmail.com>
2021-11-03 22:02:23 -0300

    feat(ResetPasswordUserController): Final Implementation.

--- src/modules/accounts/useCases/resetPasswordUser/ResetPasswordUserController.ts
+++ src/modules/accounts/useCases/resetPasswordUser/ResetPasswordUserController.ts
@@ -5,9 +5,12 @@ import { ResetPasswordUserUseCase } from "./ResetPasswordUserUseCase";
 class ResetPasswordUserController {
     async handle(request: Request, response: Response): Promise<Response> {
 
+        const { token } = request.query;
+        const { password } = request.body;
+
         const resetPasswordUserUseCase = container.resolve(ResetPasswordUserUseCase);
 
-        await resetPasswordUserUseCase.execute();
+        await resetPasswordUserUseCase.execute({ token: String(token), password });
 
         return response.send();
     };
