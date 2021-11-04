f4ff3709f58d0c49f0092c91d7eeda61484111f6
Paulo Eduardo Peixoto <pauloeduardo_peixoto@hotmail.com>
2021-11-03 22:04:04 -0300

    feat(Routes): Password Routes. Added Reset.

--- src/shared/infra/http/routes/password.routes.ts
+++ src/shared/infra/http/routes/password.routes.ts
@@ -1,3 +1,4 @@
+import { ResetPasswordUserController } from '@modules/accounts/useCases/resetPasswordUser/ResetPasswordUserController';
 import { SendForgotPasswordMailController } from '@modules/accounts/useCases/SendForgotPasswordMail/SendForgotPasswordMailController';
 import { Router } from 'express';
 
@@ -5,8 +6,9 @@ import { Router } from 'express';
 const passwordRoutes = Router();
 
 const sendForgotPasswordMailController = new SendForgotPasswordMailController();
+const resetPasswordUserController = new ResetPasswordUserController();
 
 passwordRoutes.post('/forgot', sendForgotPasswordMailController.handle)
-
+passwordRoutes.post('/reset', resetPasswordUserController.handle)
 
 export { passwordRoutes };
