73531f38a930cb18a9f12c6cc658cc229db3f3c1
Paulo Eduardo Peixoto <pauloeduardo_peixoto@hotmail.com>
2021-11-03 21:59:04 -0300

    feat(ResetPasswordUserUseCase): Final Implementation.

--- src/modules/accounts/useCases/resetPasswordUser/ResetPasswordUserUseCase.ts
+++ src/modules/accounts/useCases/resetPasswordUser/ResetPasswordUserUseCase.ts
@@ -1,6 +1,9 @@
+import { IUsersRepository } from "@modules/accounts/repositories/IUsersRepository";
 import { IUsersTokensRepository } from "@modules/accounts/repositories/IUsersTokensRepository";
+import { IDateProvider } from "@shared/container/providers/DateProvider/IDateProvider";
 import { AppError } from "@shared/errors/AppError";
 import { inject, injectable } from "tsyringe";
+import { hash } from "bcrypt";
 
 interface IRequest {
     token: string;
@@ -12,16 +15,39 @@ class ResetPasswordUserUseCase {
 
     constructor(
         @inject("UsersTokensRepository")
-        private usersTokensRepository: IUsersTokensRepository
+        private usersTokensRepository: IUsersTokensRepository,
+
+        @inject( "DayJSDateProvider")
+        private dateProvider: IDateProvider,
+
+        @inject("UsersRepository")
+        private usersRepository: IUsersRepository
     ){};
 
-    async execute({ token, password }: IRequest) {
+    async execute({ token, password }: IRequest): Promise<void> {
 
         const userToken = await this.usersTokensRepository.findByRefreshToken(token);
 
+        // Verifica se o Token informado é válido
         if(!userToken) {
             throw new AppError('Token Invalid', 404);
-        }
+        };
+
+        // Verifica se o Token não expirou
+        if(this.dateProvider.compareIfBefore(userToken.expires_date, this.dateProvider.dateNow())) {
+            throw new AppError('Token Expired', 401);
+        };
+
+        const user = await this.usersRepository.findById(userToken.id);
+
+        // Realiza a Criptografia da NOVA SENHA
+        user.password = await hash(password, 10);
+
+        // Realiza o Update do User
+        await this.usersRepository.create(user);
+
+        // Deleta o Token enviado por email, para evitar falhas de segurança
+        await this.usersTokensRepository.deleteById(userToken.id);
     };
 };
 
