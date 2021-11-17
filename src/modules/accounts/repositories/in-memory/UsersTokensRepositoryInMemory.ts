fb7452d7939931737e3b361fc03eeacaf7f592f4
Paulo Eduardo Peixoto <pauloeduardo_peixoto@hotmail.com>
2021-11-17 20:41:34 -0300

    feat(Repositories/In-Memory): create UsersTokensRepository. Method create added.

---
+++ src/modules/accounts/repositories/in-memory/UsersTokensRepositoryInMemory.ts
@@ -0,0 +1,38 @@
+import { ICreateUserTokenDTO } from "@modules/accounts/dtos/ICreateUserTokenDTO";
+import { UserTokens } from "@modules/accounts/infra/typeorm/entities/UserTokens";
+import { IUsersTokensRepository } from "../IUsersTokensRepository";
+
+
+
+class UsersTokensRepositoryInMemory implements IUsersTokensRepository {
+
+    usersTokens: UserTokens[] = [];
+
+    async create({ user_id, expires_date, refresh_token }: ICreateUserTokenDTO): Promise<UserTokens> {
+        
+        const userToken = new UserTokens();
+
+        Object.assign(userToken, {
+            expires_date,
+            refresh_token,
+            user_id
+        });
+
+        this.usersTokens.push(userToken);
+
+        return userToken;
+    };
+
+    findByUserIdAndRefreshToken(user_id: string, refresh_token: string): Promise<UserTokens> {
+        throw new Error("Method not implemented.");
+    }
+    deleteById(id: string): Promise<void> {
+        throw new Error("Method not implemented.");
+    }
+    findByRefreshToken(refresh_token: string): Promise<UserTokens> {
+        throw new Error("Method not implemented.");
+    }
+
+};
+
+export { UsersTokensRepositoryInMemory };
