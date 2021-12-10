5cbfbee7c5a0aefba83436f823b0ef5d18ab62a6
Paulo Eduardo Peixoto <pauloeduardo_peixoto@hotmail.com>
2021-12-10 20:09:10 -0300

    test(SendForgotMailUseCase): Success.

--- src/modules/accounts/useCases/SendForgotPasswordMail/SendForgotPasswordMailUseCase.spec.ts
+++ src/modules/accounts/useCases/SendForgotPasswordMail/SendForgotPasswordMailUseCase.spec.ts
@@ -1,22 +1,44 @@
 import { UsersRepositoryInMemory } from "@modules/accounts/repositories/in-memory/UsersRepositoryInMemory";
 import { UsersTokensRepositoryInMemory } from "@modules/accounts/repositories/in-memory/UsersTokensRepositoryInMemory";
 import { DayJSDateProvider } from "@shared/container/providers/DateProvider/implementations/DayJSDateProvider";
+import { MailProviderInMemory } from "@shared/container/providers/MailProvider/in-memory/MailProviderInMemory";
 import { SendForgotPasswordMailUseCase } from "./SendForgotPasswordMailUseCase"
 
 let sendForgotPasswordMailUseCase: SendForgotPasswordMailUseCase;
 let usersRepositoryInMemory: UsersRepositoryInMemory;
 let dateProvider: DayJSDateProvider;
 let usersTokensRepositoryInMemory: UsersTokensRepositoryInMemory;
+let mailProvider: MailProviderInMemory;
 
 describe("Send Forgot Mail", () => {
 
     beforeEach(() => {
+        usersRepositoryInMemory = new UsersRepositoryInMemory();
+        dateProvider = new DayJSDateProvider();
+        usersTokensRepositoryInMemory = new UsersTokensRepositoryInMemory();
+        mailProvider = new MailProviderInMemory();
         sendForgotPasswordMailUseCase = new SendForgotPasswordMailUseCase(
-
+            usersRepositoryInMemory,
+            usersTokensRepositoryInMemory,
+            dateProvider,
+            mailProvider
         )
     })
 
-    it("Should be able to send a forgot password mail to user", () => {
+    it("Should be able to send a forgot password mail to user", async () => {
 
+        //jest.spyOn() -> Função do JEST para ficar espiando ("de olho") em algo
+        const sendEmail = jest.spyOn(mailProvider, "sendMail")
+
+        await usersRepositoryInMemory.create({
+            driver_license: "635214",
+            email: "usertest@test.com",
+            name: "John Foobar",
+            password: "1234"
+        })
+
+        await sendForgotPasswordMailUseCase.execute("usertest@test.com");
+
+        expect(sendEmail).toHaveBeenCalled();
     })
 })
