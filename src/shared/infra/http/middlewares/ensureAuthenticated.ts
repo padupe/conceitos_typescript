c60154f3b7c5213edc5d3c70267414c404177900
Paulo Eduardo Peixoto <pauloeduardo_peixoto@hotmail.com>
2022-01-10 09:04:37 -0300

    refactor(ensureAuthenticated): do not use Repositories.

--- src/shared/infra/http/middlewares/ensureAuthenticated.ts
+++ src/shared/infra/http/middlewares/ensureAuthenticated.ts
@@ -1,11 +1,7 @@
 import { Request, Response, NextFunction } from "express";
 import { verify } from 'jsonwebtoken';
 import { AppError } from "@shared/errors/AppError";
-import { UsersRepository } from "@modules/accounts/infra/typeorm/repositories/UsersRepository";
-import { UsersTokensRepository } from "@modules/accounts/infra/typeorm/repositories/UsersTokensRepository";
 import auth from "@config/auth";
-
-
 
 interface IPayload {
     sub: string;
@@ -14,7 +10,6 @@ interface IPayload {
 export async function ensureAuthenticated(request: Request, response: Response, next: NextFunction) {
 
     const authHeader = request.headers.authorization;
-    const usersTokensRepository = new UsersTokensRepository();
 
     if(!authHeader){
         throw new AppError("Token Missing!", 401)
@@ -25,12 +20,7 @@ export async function ensureAuthenticated(request: Request, response: Response, 
     const [, token] = authHeader.split(" ")
 
     try {
-        const { sub: user_id } = verify(token, auth.secret_refresh_token) as IPayload;
-
-        const user = usersTokensRepository.findByUserIdAndRefreshToken(user_id, token);
-        if (!user) {
-            throw new AppError("User does not exists!", 401)
-        };
+        const { sub: user_id } = verify(token, auth.secret_token) as IPayload;
 
         request.user = {
             id: user_id
