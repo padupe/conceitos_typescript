fe177d092718f6ec50d6a7b46bfda952ce67bd14
padupe <pauloeduardo_peixoto@hotmail.com>
2022-01-12 20:35:34 -0300

    feat(LocalStorageProvider): added to the Project. Dir: src/shared/container/providers/StorageProvider/implementations.

---
+++ src/shared/container/providers/StorageProvider/implementations/LocalStorageProvider.ts
@@ -0,0 +1,16 @@
+import { IStorageProvider } from "../IStorageProvider";
+
+
+
+class LocalStorageProvider implements IStorageProvider {
+    save(file: string): Promise<string> {
+        throw new Error("Method not implemented.");
+    }
+    delete(file: string): Promise<void> {
+        throw new Error("Method not implemented.");
+    }
+
+    
+};
+
+export { LocalStorageProvider };
