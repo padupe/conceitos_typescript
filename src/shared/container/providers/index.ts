7f648986cc052dcb78bb2e686649853d7c8074d0
padupe <pauloeduardo_peixoto@hotmail.com>
2022-01-12 20:59:46 -0300

    feat(index/Providers): added container StorageProvider. Dir: src/shared/container/providers.

--- src/shared/container/providers/index.ts
+++ src/shared/container/providers/index.ts
@@ -3,6 +3,8 @@ import { IDateProvider } from './DateProvider/IDateProvider';
 import { DayJSDateProvider } from './DateProvider/implementations/DayJSDateProvider';
 import { IMailProvider } from './MailProvider/IMailProvider';
 import { EtherealMailProvider } from './MailProvider/implementations/EtherealMailProvider';
+import { LocalStorageProvider } from './StorageProvider/implementations/LocalStorageProvider';
+import { IStorageProvider } from './StorageProvider/IStorageProvider';
 
 container.registerSingleton<IDateProvider>(
     "DayJSDateProvider",
@@ -16,4 +18,9 @@ container.registerInstance<IMailProvider>(
     "EtherealMailProvider",
     new EtherealMailProvider()
     // EtherealMailProvider
+);
+
+container.registerSingleton<IStorageProvider>(
+    "StorageProvider",
+    LocalStorageProvider
 );
