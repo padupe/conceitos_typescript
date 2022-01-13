cc9e81a992ed97a90c3d9186147ff03b3f8fd164
Paulo Eduardo Peixoto <pauloeduardo_peixoto@hotmail.com>
2022-01-13 19:44:03 -0300

    fix(index/container): change LocalStorageProvider to S3StorageProvider. For Initial Tests.

--- src/shared/container/providers/index.ts
+++ src/shared/container/providers/index.ts
@@ -4,6 +4,7 @@ import { DayJSDateProvider } from './DateProvider/implementations/DayJSDateProvi
 import { IMailProvider } from './MailProvider/IMailProvider';
 import { EtherealMailProvider } from './MailProvider/implementations/EtherealMailProvider';
 import { LocalStorageProvider } from './StorageProvider/implementations/LocalStorageProvider';
+import { S3StorageProvider } from './StorageProvider/implementations/S3StorageProvider';
 import { IStorageProvider } from './StorageProvider/IStorageProvider';
 
 container.registerSingleton<IDateProvider>(
@@ -22,5 +23,5 @@ container.registerInstance<IMailProvider>(
 
 container.registerSingleton<IStorageProvider>(
     "StorageProvider",
-    LocalStorageProvider
+    S3StorageProvider
 );
