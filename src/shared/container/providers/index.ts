47d5037a73d72d4f2de63595a3f6c1ae16615638
Paulo Eduardo Peixoto <pauloeduardo_peixoto@hotmail.com>
2022-01-16 19:32:12 -0300

    refactor(index): Clean code. Create new file to MailProvider. Dir: src/shared/container/providers.

--- src/shared/container/providers/index.ts
+++ src/shared/container/providers/index.ts
@@ -1,8 +1,6 @@
 import { container } from 'tsyringe';
 import { IDateProvider } from './DateProvider/IDateProvider';
 import { DayJSDateProvider } from './DateProvider/implementations/DayJSDateProvider';
-import { IMailProvider } from './MailProvider/IMailProvider';
-import { EtherealMailProvider } from './MailProvider/implementations/EtherealMailProvider';
 import { LocalStorageProvider } from './StorageProvider/implementations/LocalStorageProvider';
 import { S3StorageProvider } from './StorageProvider/implementations/S3StorageProvider';
 import { IStorageProvider } from './StorageProvider/IStorageProvider';
@@ -12,19 +10,10 @@ container.registerSingleton<IDateProvider>(
     DayJSDateProvider
 );
 
-// registerInstance, pois o serviço precisa ser iniciado em conjunto com a Aplicação
-// Código comentado pois não estava funcionando correatamente nas linhas 14 e 17.
-// container.registerInstance<IMailProvider>(
-container.registerSingleton<IMailProvider>(
-    "EtherealMailProvider",
-    // new EtherealMailProvider()
-    EtherealMailProvider
-);
-
 const diskStorage = {
     local: LocalStorageProvider,
     s3: S3StorageProvider
-}
+};
 
 container.registerSingleton<IStorageProvider>(
     "StorageProvider",
