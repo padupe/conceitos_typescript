01f5600adc58208ea9af239c7b312c179f14b210
Paulo Eduardo Peixoto <pauloeduardo_peixoto@hotmail.com>
2022-01-16 19:37:27 -0300

    refactor(index): Clean code. Create new file to StorageProvider. Dir: src/shared/container/providers.

--- src/shared/container/providers/index.ts
+++ src/shared/container/providers/index.ts
@@ -1,21 +1,8 @@
 import { container } from 'tsyringe';
 import { IDateProvider } from './DateProvider/IDateProvider';
 import { DayJSDateProvider } from './DateProvider/implementations/DayJSDateProvider';
-import { LocalStorageProvider } from './StorageProvider/implementations/LocalStorageProvider';
-import { S3StorageProvider } from './StorageProvider/implementations/S3StorageProvider';
-import { IStorageProvider } from './StorageProvider/IStorageProvider';
 
 container.registerSingleton<IDateProvider>(
     "DayJSDateProvider",
     DayJSDateProvider
-);
-
-const diskStorage = {
-    local: LocalStorageProvider,
-    s3: S3StorageProvider
-};
-
-container.registerSingleton<IStorageProvider>(
-    "StorageProvider",
-    diskStorage[process.env.DISK]
 );
