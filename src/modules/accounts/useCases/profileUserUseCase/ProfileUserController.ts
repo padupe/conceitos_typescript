5516726b852f4c2dd4d7fbcf5a78b7912795a696
Paulo Eduardo Peixoto <pauloeduardo_peixoto@hotmail.com>
2022-01-16 15:34:32 -0300

    feat(ProfileUserController): Insert function handle. Dir: src/modules/accounts/useCases/profileUserUseCase.

--- src/modules/accounts/useCases/profileUserUseCase/ProfileUserController.ts
+++ src/modules/accounts/useCases/profileUserUseCase/ProfileUserController.ts
@@ -1,7 +1,11 @@
-
+import { Request, Response} from 'express';
+import { container } from 'tsyringe';
 
 class ProfileUserController {
 
+    async handle(request: Request, response: Response): Promise<Response> {
+        return
+    };
 };
 
 export { ProfileUserController };
