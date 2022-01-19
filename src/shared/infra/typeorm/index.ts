7e191de3c089910f80b6fdbcbc6e211b3cc78d3c
Paulo Eduardo Peixoto <pauloeduardo_peixoto@hotmail.com>
2022-01-19 19:48:02 -0300

    fix(index): fix connection Database for Deploy. Dir: src/shared/infra/typeorm.

--- src/shared/infra/typeorm/index.ts
+++ src/shared/infra/typeorm/index.ts
@@ -5,36 +5,36 @@ interface IOptions {
 }
 
 // Mudança no Código para setar um Usuário Admin
-const createConnectionDB = getConnectionOptions().then(options => {
+// const createConnectionDB = getConnectionOptions().then(options => {
 
-    let hostDB = 'localhost'
+//     let hostDB = 'localhost'
 
-    if(process.env.NODE_ENV === 'test') {
-        hostDB = 'rentx'
-    }
+//     if(process.env.NODE_ENV === 'test') {
+//         hostDB = 'rentx'
+//     }
 
-    const newOptions = options as IOptions;
-    newOptions.host = hostDB;
-    // A opção abaixo não estava funcionando corretamente com Container/Docker
-    // Alterei para 'localhost' para apenas o DB rodar em Docker
-    // newOptions.host = 'localhost';
-    createConnection({
-        ...options,
-    });
-});
+//     const newOptions = options as IOptions;
+//     newOptions.host = hostDB;
+//     // A opção abaixo não estava funcionando corretamente com Container/Docker
+//     // Alterei para 'localhost' para apenas o DB rodar em Docker
+//     // newOptions.host = 'localhost';
+//     createConnection({
+//         ...options,
+//     });
+// });
 
-export { createConnectionDB };
+// export { createConnectionDB };
 
-// export default async(host = 'localhost'): Promise<Connection> => {
-//     const defaultOptions = await getConnectionOptions();
+// Durate o Chapter VI - Alteramos o código abaixo para realizar o Deploy
+export default async(): Promise<Connection> => {
+    const defaultOptions = await getConnectionOptions();
 
-//     return createConnection(
-//         Object.assign(defaultOptions, {
-//             host,
-//             database:
-//                 process.env.NODE_ENV === 'test'
-//                     ? 'rentx_test'
-//                     : defaultOptions.database    
-//         })
-//     )
-// }
+    return createConnection(
+        Object.assign(defaultOptions, {
+            database:
+                process.env.NODE_ENV === 'test'
+                    ? 'rentx_test'
+                    : defaultOptions.database    
+        })
+    )
+}
